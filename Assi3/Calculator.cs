e6cd3cd1713b1f0a63e525e63f46a7920f4b33ed
skumar <skumar@BMINP5722-2304>
2023-04-20 21:48:17 +0530

    Add project files.

---
+++ Assi3/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            int Add = a+b;
+            return Add;
+        }
+        public int subs(int a, int b)
+        {
+            int subs = a - b;
+            return subs;
+        }
+        public int Mul(int a, int b)
+        {
+            int Mul = a * b;
+            return Mul;
+        }
+        public float div(int a, int b)
+        {
+            float div = a / b;
+            return div;
+        }
+
+    }
+}
---
+++ Assi3/Program.cs
@@ -0,0 +1,26 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assi3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            int Add = calculator.Add(6, 6);
+            int Subs = calculator.subs(10, 5);
+            int Mul = calculator.Mul(10, 5);
+            float div= calculator.div(10, 5);
+            Console.WriteLine(Add);
+            Console.WriteLine(Subs);
+            Console.WriteLine(Mul);
+            Console.WriteLine(div);
+            Console.ReadLine();
+        }
+    }
+}
